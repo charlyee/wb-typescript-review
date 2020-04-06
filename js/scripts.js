8612b6d1eb381ad7af8e8f2a18e636b61dc2b971
Warren Uhrich <hello@warren.codes>
2020-04-06 13:53:13 -0600

    Add Calculator class.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var Calculator = /** @class */ (function () {
+    function Calculator() {
+        var form = document.querySelector('#calculator');
+        var num1 = document.querySelector('[name="num1"]');
+        var num2 = document.querySelector('[name="num2"]');
+        var result = document.querySelector('#calculator strong');
+    }
+    return Calculator;
+}());
---
+++ src/ts/Calculator.Class.ts
@@ -0,0 +1,9 @@
+class Calculator {
+  constructor () {
+    const form: HTMLElement = document.querySelector( '#calculator' );
+    const num1: HTMLInputElement = document.querySelector( '[name="num1"]' );
+    const num2: HTMLInputElement = document.querySelector( '[name="num2"]' );
+    const result: HTMLElement = document.querySelector( '#calculator strong' );
+  }
+  
+}
