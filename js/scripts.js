bbf5d00e4da759e11c7ec12d55215da02fe0a9db
Warren Uhrich <hello@warren.codes>
2020-04-06 14:14:54 -0600

    Add methods and properties to calculator class.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,46 @@
 var Calculator = /** @class */ (function () {
     function Calculator() {
-        var form = document.querySelector('#calculator');
-        var num1 = document.querySelector('[name="num1"]');
-        var num2 = document.querySelector('[name="num2"]');
-        var result = document.querySelector('#calculator strong');
+        // Properties:
+        this.form = document.querySelector('#calculator');
+        this.num1 = document.querySelector('[name="num1"]');
+        this.num2 = document.querySelector('[name="num2"]');
+        this.operator = document.querySelector('[name="operator"]');
+        this.result = document.querySelector('#calculator strong');
     }
+    // Methods:
+    Calculator.prototype.add = function (num1, num2) {
+        return num1 + num2;
+    };
+    Calculator.prototype.subtract = function (num1, num2) {
+        return num1 - num2;
+    };
+    Calculator.prototype.multiply = function (num1, num2) {
+        return num1 * num2;
+    };
+    Calculator.prototype.divide = function (num1, num2) {
+        return num1 / num2;
+    };
+    Calculator.prototype.remainder = function (num1, num2) {
+        return num1 % num2;
+    };
+    Calculator.prototype.getOperator = function () {
+        return this.operator.value;
+    };
+    Calculator.prototype.calculate = function () {
+        var num1 = Number(this.num1.value);
+        var num2 = Number(this.num2.value);
+        switch (this.getOperator()) {
+            case 'addition':
+                return this.add(num1, num2);
+            case 'subtraction':
+                return this.subtract(num1, num2);
+            case 'multiplication':
+                return this.multiply(num1, num2);
+            case 'division':
+                return this.divide(num1, num2);
+            case 'remainder':
+                return this.remainder(num1, num2);
+        }
+    };
     return Calculator;
 }());
--- src/ts/Calculator.Class.ts
+++ src/ts/Calculator.Class.ts
@@ -1,9 +1,43 @@
 class Calculator {
-  constructor () {
-    const form: HTMLElement = document.querySelector( '#calculator' );
-    const num1: HTMLInputElement = document.querySelector( '[name="num1"]' );
-    const num2: HTMLInputElement = document.querySelector( '[name="num2"]' );
-    const result: HTMLElement = document.querySelector( '#calculator strong' );
+  // Properties:
+  form: HTMLElement = document.querySelector( '#calculator' );
+  num1: HTMLInputElement = document.querySelector( '[name="num1"]' );
+  num2: HTMLInputElement = document.querySelector( '[name="num2"]' );
+  operator: HTMLSelectElement = document.querySelector( '[name="operator"]' );
+  result: HTMLElement = document.querySelector( '#calculator strong' );
+  // Methods:
+  add ( num1: number, num2: number ): number {
+    return num1 + num2;
   }
-  
+  subtract ( num1: number, num2: number ): number {
+    return num1 - num2;
+  }
+  multiply ( num1: number, num2: number ): number {
+    return num1 * num2;
+  }
+  divide ( num1: number, num2: number ): number {
+    return num1 / num2;
+  }
+  remainder ( num1: number, num2: number ): number {
+    return num1 % num2;
+  }
+  getOperator (): string {
+    return this.operator.value;
+  }
+  calculate (): number {
+    const num1: number = Number( this.num1.value );
+    const num2: number = Number( this.num2.value );
+    switch ( this.getOperator() ) {
+      case 'addition':
+        return this.add( num1, num2 );
+      case 'subtraction':
+        return this.subtract( num1, num2 );
+      case 'multiplication':
+        return this.multiply( num1, num2 );
+      case 'division':
+        return this.divide( num1, num2 );
+      case 'remainder':
+        return this.remainder( num1, num2 );
+    }
+  }
 }
